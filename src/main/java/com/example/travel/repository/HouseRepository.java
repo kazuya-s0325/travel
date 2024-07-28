0e0c9bfc7ce39056b4a159670d506904027034ca
kazuya-s0325 <kaz.keyboardist@gmail.com>
2024-07-28 19:39:21 +0900

    added

--- src/main/java/com/example/travel/repository/HouseRepository.java
+++ src/main/java/com/example/travel/repository/HouseRepository.java
@@ -1,8 +1,10 @@
 package com.example.travel.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.travel.entity.House;
 
 public interface HouseRepository extends JpaRepository<House, Integer> {
-
+  public Page<House> findByNameLike(String keyword, Pageable pageable);
 }
