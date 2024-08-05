a7ca022c53ebf5d993b46a116be27a37b10583ac
Kazuya Shimazaki <kaz.keyboardist@gmail.com>
2024-08-05 18:07:38 +0900

    added

---
+++ src/main/java/com/example/travel/security/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.travel.security;
+
+import java.util.Collection;
+import java.util.ArrayList;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.example.travel.entity.User;
+import com.example.travel.repository.UserRepository;
+
+@Servive
+public class UserDetailsServiceImpl implements UserDetailsService {
+  private final UserRepository userRepository;
+
+  public UserDetailsServiceImpl(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    try {
+      User user = userRepository.findByEmail(email);
+      String userRoleName = user.getRole().getName();
+      Collection<GrantedAuthority> authorities = new ArrayList<>();
+    } catch (Exception e) {
+      throw new UsernameNotFoundException("user not found.");
+    }
+  }
+
+}
